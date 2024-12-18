dff3126bf3bb4c163426126b69ef8eb7f2e47879
Nguyen Ngoc Anh Tuan <Tomtepfa@gmail.com>
2024-12-18 18:28:16 +0700

    fix/cors: add cors origins

--- src/configs/corsConfig.ts
+++ src/configs/corsConfig.ts
@@ -1,6 +1,8 @@
 export const corsConfig = {
   origin: [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://contest.vinhuniversity.edu.vn',
+    'http://contest.vinhuniversity.edu.vn'
 ],
   credentials: true,
 };
